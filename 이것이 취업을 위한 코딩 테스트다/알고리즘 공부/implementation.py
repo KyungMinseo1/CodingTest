281cf1aab174c26e9341fe63a3e82f92ddcc1eab
KyungMinseo1 <kminxi0217@g.skku.edu>
2025-12-30 17:36:22 +0900

    [이코테] implement practice

--- 이것이 취업을 위한 코딩 테스트다/알고리즘 공부/implementation.py
+++ 이것이 취업을 위한 코딩 테스트다/알고리즘 공부/implementation.py
@@ -19,7 +19,49 @@ def udlr():
     return str(loc[0]) + " " + str(loc[1])
 
 # 시각
+def time():
+    n = int(input())
+    valid_in_60 = 0
 
+    for minute in range(60):
+        if "3" in str(minute):
+            valid_in_60 += 1
+
+    total_valid = valid_in_60 * 60 # for valid minute
+    not_valid_min = 60-valid_in_60 # not valid minutes
+    extra_valid = not_valid_min*valid_in_60
+
+    total_valid += extra_valid
+
+    total = 0
+    for h in range(n+1):
+        if "3" in str(h):
+            total += 60*60
+        else:
+            total += total_valid
+
+    return total
+
+def knight(): # ord() 쓰는 방식도 존재
+    loc = str(input())
+    x_loc, y_loc = loc[0], loc[1]
+
+    x = ['a', 'b', 'c', 'd', 'e', 'f', 'g', 'h']
+    y = ['1', '2', '3', '4', '5', '6', '7', '8']
+
+    dx = [-2, -2, 2, 2, -1, 1, -1, 1]
+    dy = [-1, 1, -1, 1, -2, -2, 2, 2]
+
+    current_loc = [x.index(x_loc), y.index(y_loc)]
+    count = 0
+
+    for cdx, cdy in zip(dx, dy):
+        ndx, ndy = current_loc[0]+cdx, current_loc[1]+cdy
+        if ndx < 0 or ndx > len(x)-1 or ndy < 0 or ndy > len(y)-1:
+            continue
+        count += 1
+
+    return count
 
 if __name__=="__main__":
-    print(udlr())
+    print(knight())
